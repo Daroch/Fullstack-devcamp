8621ad9fceb2612b0e6594da8346dfbc442201e1
Daroch <daroch314@gmail.com>
2023-06-14 05:02:22 +0200

    python web scraper v2

--- python_exercises/python_package_project.py
+++ python_exercises/python_package_project.py
@@ -5,13 +5,12 @@ from bs4 import BeautifulSoup
 
 
 def get_urls():
-    url = 'https://api.dailysmarty.com/posts'
+    url = 'https://dailysmarty.com/posts'
     r = requests.get(url)
-    #soup = BeautifulSoup(r.json(), 'html.parser')
-    #print(soup.find_all('url_for_post'))
-    posts = r.json()['posts']
-    for post in posts:
-        endpoint = post['url_for_post'].rsplit('/', 1)[-1]
+    soup = BeautifulSoup(r.text, 'html.parser')
+    links = soup.find_all(attrs={"data-turbolinks": "false"})
+    for link in links:
+        endpoint = link['href'].rsplit('/', 1)[-1]
         print('"'+str(inflection.titleize(endpoint))+'"')
     
 get_urls()
